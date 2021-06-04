ec1f358e7badbdcba58304f48c96b36f26580d47
honggom <cavok699@naver.com>
2021-06-04 23:33:43 +0900

    2021-06-04

--- 2021-06-03/Double_Linked_List.py
+++ 2021-06-03/Double_Linked_List.py
@@ -1,15 +1,15 @@
 class Node():
 
-    def __init__(self, data, prev, next):
+    def __init__(self, data, prev=None, next=None):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev = prev
+        self.next = next
 
 
 class DLL():
 
     def __init__(self, data):
-        self.head = Node(data, None, None)
+        self.head = Node(data)
         self.tail = self.head
 
     def insert(self, data):
@@ -17,6 +17,16 @@ class DLL():
         self.tail.next.prev = self.tail
         self.tail = self.tail.next
 
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
 
-dll = DLL(4)
+
+dll = DLL(1)
+dll.insert(2)
+dll.insert(3)
+dll.insert(4)
 dll.insert(5)
+dll.desc()
