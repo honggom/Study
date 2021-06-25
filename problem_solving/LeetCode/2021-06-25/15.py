b9c52aabef8dd76d813bd2e2fa180347a5464658
honggom <cavok699@naver.com>
2021-06-25 15:22:23 +0900

    2021-06-25

--- problem_solving/LeetCode/2021-06-25/15.py
+++ problem_solving/LeetCode/2021-06-25/15.py
@@ -4,21 +4,28 @@ Output: [[-1,-1,2],[-1,0,1]]
 '''
 
 def sum(nums:list):
-    result = {}
+    result = []
     nums.sort()
     for i in range(len(nums)-1):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
         left = i + 1
         right = len(nums) - 1
         while left < right:
             num = nums[i] + nums[left] + nums[right]
-            if num == 0:
-                result[str(nums[i])+str(nums[left])+str(nums[right])] = [nums[i], nums[left], nums[right]]
-                break
+            if num < 0:
+                left += 1
             elif num > 0:
                 right -= 1
             else:
+                result.append([nums[i], nums[left], nums[right]])
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
                 left += 1
-    return list(result.values())
+                right -= 1
+    return result
 
 #l = [-1, 0, 1, 2, -1, -4]
 l = [-2,0,1,1,2]
