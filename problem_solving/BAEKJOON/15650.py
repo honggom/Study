801c1ae3e68b441e8e219e489d0c8085f0268ce4
honggom <cavok699@naver.com>
2021-07-09 23:12:28 +0900

    2021-07-09

---
+++ problem_solving/BAEKJOON/15650.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+visited = []
+
+
+def dfs(start):
+    if len(visited) == m:
+        print(' '.join(map(str, visited)))
+        return
+
+    for i in range(start, n + 1):
+        if i not in visited:
+            visited.append(i)
+            dfs(i + 1)
+            visited.pop()
+
+dfs(1)
