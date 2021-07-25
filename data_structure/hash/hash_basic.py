58b030502e9228ef86b6ecbb44b0231419ff20eb
honggom <cavok699@naver.com>
2021-07-25 22:44:19 +0900

    2021-07-25 ps

--- data_structure/hash/hash_basic.py
+++ data_structure/hash/hash_basic.py
@@ -7,26 +7,29 @@ hash_table = [0 for i in range(8)]
 '''
 
 
-def change_to_hash(data):
-    return hash(data)
+def change_to_hash(key):
+    return hash(key)
 
 
-def hash_func(key):
+def hash_function(key):
     return key % 8
 
 
 def save_data(key, value):
-    hash_addr = hash_func(change_to_hash(key))
-    print(hash_addr)
-    hash_table[hash_addr] = value
+    hash_address = hash_function(change_to_hash(key))
+    hash_table[hash_address] = value
 
 
 def read_data(data):
-    hash_addr = hash_func(change_to_hash(data))
-    return hash_table[hash_addr]
+    hash_address = hash_function(change_to_hash(data))
+    return hash_table[hash_address]
 
 
 save_data("dave", 2123132)
 save_data("hong", 112312)
-print(hash_table)
 print(read_data("hong"))
+
+# 결과
+# >>> 112312
+
+print(change_to_hash("abvasd"))
