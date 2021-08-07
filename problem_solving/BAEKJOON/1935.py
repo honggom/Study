1e19fcfe5a5b0e6660999b0d7d8adea87465eb3d
honggom <cavok699@naver.com>
2021-08-07 23:51:28 +0900

    2021-08-07 ps

---
+++ problem_solving/BAEKJOON/1935.py
@@ -0,0 +1,26 @@
+n = int(input())
+question = input()
+nums = {}
+stack = []
+cal = ["+", "-", "*", "/"]
+
+for unicode_num in range(65, 65+n):
+    nums[chr(unicode_num)] = int(input())
+
+for q in question:
+    if q in cal:
+        num1, num2 = stack.pop(), stack.pop()
+        if q == "+":
+            stack.append(num1 + num2)
+        elif q == "-":
+            stack.append(num1 - num2)
+        elif q == "*":
+            stack.append(num1 * num2)
+        elif q == "/":
+            stack.append(num1 % num2)
+    else:
+        stack.append(nums[q])
+    print(stack)
+
+#print(stack.pop())
+
