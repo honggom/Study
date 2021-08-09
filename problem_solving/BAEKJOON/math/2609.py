8ec1abf33e901ea87beeabceb52984e1d808486f
honggom <cavok699@naver.com>
2021-08-09 16:24:07 +0900

    2021-08-09 ps2

---
+++ problem_solving/BAEKJOON/math/2609.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+# gcd : Greatest common divisor
+# lcm : least common multiple
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def lcm(a, b):
+    g = gcd(a, b)
+    return int(g * (a/g) * (b/g))
+
+
+
+print(gcd(a, b))
+print(lcm(a, b))
