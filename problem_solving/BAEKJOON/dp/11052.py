18b38b8c8d1ba4e5fe8a0a0c31baf5b664dbc757
honggom <cavok699@naver.com>
2021-08-12 09:53:03 +0900

    2021-08-12 ps

--- problem_solving/BAEKJOON/dp/11052.py
+++ problem_solving/BAEKJOON/dp/11052.py
@@ -5,7 +5,6 @@ cache[1] = prices[1]
 
 for i in range(2, n + 1):
     for j in range(1, i + 1):
-        if cache[i] < cache[i - j] + prices[j]:
-            cache[i] = cache[i - j] + prices[j]
+        cache[i] = max(cache[i], cache[i - j] + prices[j])
 
 print(cache[n])
