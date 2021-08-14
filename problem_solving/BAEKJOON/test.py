b15b913797e58263074a060b4fc8929d8d0de742
honggom <cavok699@naver.com>
2021-08-14 16:12:40 +0900

    test

--- problem_solving/BAEKJOON/test.py
+++ problem_solving/BAEKJOON/test.py
@@ -1,18 +1,1 @@
-fruitWeights = [30, 40, 10, 20, 30]
-k = 3
-
-def solution(fruitWeights, k):
-    n = 0
-    result = set()
-    max_len = len(fruitWeights) + 1
-
-    while True:
-        result.add(max(fruitWeights[n:k]))
-        n += 1
-        k += 1
-        if k == max_len:
-            break
-
-    return sorted(result, reverse=True)
-
-print(solution(fruitWeights, k))
+tk
