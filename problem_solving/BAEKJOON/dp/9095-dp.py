f06132b80c399b0b947a3e27f3f7803c74259e4b
변재홍 <honggom@byeonjaehong-ui-MacBookPro.local>
2021-08-15 16:27:23 +0900

    2021-08-15 ps

---
+++ problem_solving/BAEKJOON/dp/9095-dp.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+
+    n = int(input())
+    dp = [0] * 11
+    dp[1], dp[2], dp[3] = 1, 2, 4
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[n])
+
