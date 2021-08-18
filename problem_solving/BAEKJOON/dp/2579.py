d6969d52bfbd100d5a4a038fc3c81ecdaa8b5bc9
JaeHong Byeon <cavok699@naver.com>
2021-08-18 17:51:58 +0900

    edit typo

--- problem_solving/BAEKJOON/dp/2579.py
+++ problem_solving/BAEKJOON/dp/2579.py
@@ -11,5 +11,4 @@ dp[2] = max(steps[1] + steps[2], steps[0] + steps[2])
 for i in range(3, n):
     dp[i] = max(dp[i - 3] + steps[i - 1] + steps[i], dp[i - 2] + steps[i])
 
-print(dp)
 print(dp[n - 1])
