99c9e15219dbd0277107e6b494716bb14b46dca6
JaeHong Byeon <cavok699@naver.com>
2021-08-20 16:00:31 +0900

    refactor: Refactoring 1158.py
    코드 간소화

--- problem_solving/BAEKJOON/queue/1158.py
+++ problem_solving/BAEKJOON/queue/1158.py
@@ -1,11 +1,12 @@
 from collections import deque
-n, k = map(int, input().split())
-dq = deque([i+1 for i in range(n)])
+import sys
 
+n, k = map(int, sys.stdin.readline().split())
+dq = deque([i+1 for i in range(n)])
 result = []
+
 while dq:
     dq.rotate(-(k-1))
-    result.append(dq.popleft())
-print("<", end="")
-[print(result[i], end=", ") for i in range(len(result)) if i != len(result) - 1]
-print(str(result[len(result)-1])+">")
+    result.append(str(dq.popleft()))
+
+sys.stdout.write("<"+", ".join(result)+">")
