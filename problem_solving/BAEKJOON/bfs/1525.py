7fc6846b9e68a156462991ebb3f35e4cd3b94185
honggom <cavok699@naver.com>
2021-09-03 09:33:55 +0900

    refactor: Update ac code

--- problem_solving/BAEKJOON/bfs/1525.py
+++ problem_solving/BAEKJOON/bfs/1525.py
@@ -1,35 +1,46 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-given_puzzle = [list(input().split()) for _ in range(3)]
-goal = '123456780'
-history = []
 
-def normalize(puzzle):
-    normalized_puzzle = ""
-    for p in puzzle:
-        normalized_puzzle += "".join(p)
-    return normalized_puzzle
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-def search(puzzle, count):
-    normalized_puzzle = normalize(puzzle)
-    if normalized_puzzle == goal:
-        print(count)
+start = ""
+for _ in range(3):
+    temp = input().strip()
+    temp = temp.replace(" ", "")
+    start += temp
+
+start = start.replace("0", "9")
+
+q = deque()
+q.append(start)
+
+count = dict()
+count[start] = 0
+
+while q:
+    now = q.popleft()
+    if now == "123456789":
+        print(count[now])
         sys.exit(0)
-    if normalized_puzzle in history:
-        return
-    history.append(normalized_puzzle)
-    try:
-        for i in range(3):
-            for j in range(3):
-                if puzzle[i][j] == "0":
-                    # 위 puzzle[i - 1][j], puzzle[i][j] = puzzle[i][j], puzzle[i - 1][j]
-                    # 아래
-                    # 왼쪽
-                    # 오른쪽
-                    search()
-                    search()
-                    search()
-                    search()
-    except:
-        print("can't go")
 
+    pos = now.find("9")
+    x = pos // 3 # 몫
+    y = pos % 3  # 나머지
+
+    for i in range(4):
+        new_x = x + dx[i]
+        new_y = y + dy[i]
+
+        if 0 <= new_x < 3 and 0 <= new_y < 3:
+            new_pos = new_x * 3 + new_y
+            next_num = list(now)
+            next_num[new_pos], next_num[pos] = next_num[pos], next_num[new_pos]
+            next_num = "".join(next_num)
+
+            if not count.get(next_num):
+                q.append(next_num)
+                count[next_num] = count[now] + 1
+
+print("-1")
