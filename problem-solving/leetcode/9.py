21c3682c7c5d5b1a58a312fcf3e397f819549947
변재홍 <cavok699@naver.com>
2021-10-25 22:49:18 +0900

    refactor: Edit

--- problem-solving/leetcode/9.py
+++ problem-solving/leetcode/9.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
         if "".join(reversed(str(x))) == str(x):
             return True
         return False
