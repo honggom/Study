a02346881e1a6a5648e67596be7c2a58df205aa9
honggom <cavok699@naver.com>
2021-12-28 16:16:23 +0900

    [백준] 14716 현수막

---
+++ problem-solving/baekjoon/graph/14716.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+mtx = [list(map(int, input().split())) for _ in range(n)]
+
+def dfs(i, j):
+    if i < 0 or i >= n or j < 0 or j >= m or mtx[i][j] == 0:
+        return
+    mtx[i][j] = 0
+
+    dfs(i - 1, j)
+    dfs(i, j - 1)
+    dfs(i + 1, j)
+    dfs(i, j + 1)
+    dfs(i - 1, j - 1)
+    dfs(i + 1, j + 1)
+    dfs(i - 1, j + 1)
+    dfs(i + 1, j - 1)
+
+
+count = 0
+
+for i in range(n):
+    for j in range(m):
+        if mtx[i][j] == 1:
+            dfs(i, j)
+            count += 1
+
+print(count)
